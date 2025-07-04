74f8bcc403f08f15a5369cbc583e93e479fbe275
Krasimir <krasimir.st.tsonev@gmail.com>
2025-07-04 23:04:01 +0300

    Progress on the graph resolving

--- forket/lib/graph.js
+++ forket/lib/graph.js
@@ -7,10 +7,11 @@ const { CachedInputFileSystem, ResolverFactory } = require("enhanced-resolve");
 const traverseNode = require("./utils/traverseNode.js");
 
 const VALID_ENTRY_POINTS = [".js", ".jsx", ".ts", ".tsx"];
+const DEBUGGING_MODULE_RESOLVING = false;
 
 async function processFile(file) {
   const code = fs.readFileSync(file, 'utf8');
-  const { transform, parse, print } = swc;
+  const { parse } = swc;
   const ast = await parse(code, {
     syntax: "typescript", // or 'ecmascript'
     tsx: true,
@@ -73,6 +74,49 @@ async function resolveImports(host, request) {
     });
   });
 }
+async function processEntryPoint(entryPoint) {
+  const PROCESSED = new Map();
+  const RESOLVED = new Map();
+
+  async function process(filePath) {
+    DEBUGGING_MODULE_RESOLVING && console.log("Processing:", filePath);
+    let file = PROCESSED.get(filePath);
+    if (!file) {
+      file = await processFile(filePath);
+      PROCESSED.set(filePath, file);
+    } else {
+      DEBUGGING_MODULE_RESOLVING && console.log(`File "${filePath}" is already processed, skipping.`);
+      return;
+    }
+    DEBUGGING_MODULE_RESOLVING && console.log(file.imports);
+    for (let j = 0; j < file.imports.length; j++) {
+      const imp = file.imports[j];
+      if (!imp.source) {
+        continue;
+      }
+      const key = `${path.dirname(filePath)}:${imp.source}`;
+      try {
+        let resolved = RESOLVED.get(key);
+        if (typeof resolved === "undefined") {
+          resolved = await resolveImports(filePath, imp.source);
+          RESOLVED.set(key, resolved);
+        }
+        if (resolved !== null) {
+          file.children.push(await process(resolved));
+        } else {
+          DEBUGGING_MODULE_RESOLVING && console.log(`Ignoring ${imp.source}`);
+        }
+      } catch (err) {
+        DEBUGGING_MODULE_RESOLVING && console.log(`Ignoring ${imp.source}`);
+        RESOLVED.set(key, null);
+      }
+    }
+    return file;
+  }
+
+  return process(entryPoint);
+}
+
 
 module.exports = async function (dir) {
   // finding the entry points for processing
@@ -84,250 +128,10 @@ module.exports = async function (dir) {
     .map((entry) => path.join(dir, entry.name));
 
   // resolving the imports and building the graph
-  const PROCESSED = new Map();
-  const RESOLVED = new Map();
-  await Promise.all(entryPoints.map(async (entryPoint) => {
-    async function process(filePath) {
-      let file = PROCESSED.get(filePath);
-      if (!file) {
-        file = await processFile(filePath);
-        PROCESSED.set(filePath, file);
-      }
-      console.log(`---> ${filePath}`);
-      return Promise.all(file.imports.map(async (imp) => {
-        const key = `${path.dirname(filePath)}:${imp.source}`;
-        try {
-          let resolved = RESOLVED.get(key);
-          if (typeof resolved === 'undefined') {
-            resolved = await resolveImports(filePath, imp.source);
-            RESOLVED.set(key, resolved);
-          } else if (resolved === null) {
-            return;
-          }
-          console.log(`:${imp.source}`);
-          console.log(`it is in ${resolved}`);
-          file.children.push(await process(resolved));
-        } catch(err) {
-          RESOLVED.set(key, null);
-          console.error(`Ignoring "${imp.source}"`);
-        }
-      }));
-    }
-    return process(entryPoint);
-  }));
-}
-
-/*
-const { CachedInputFileSystem, ResolverFactory } = require("enhanced-resolve");
-const Fs = require("fs");
-const Path = require('path');
-const Module = require('module');
-
-async function buildGraph(entrypoint, options) {
-  const fileSystem = new CachedInputFileSystem(options.fs ?? Fs, 4000);
-  const resolver = ResolverFactory.createResolver({
-    pnpApi: null,
-    conditionNames: options.conditionNames ?? ['import', 'require', 'default'],
-    useSyncFileSystemCalls: false,
-    fileSystem,
-    extensions: options.resolveExtensions ?? [
-      '.js',
-      '.jsx',
-      '.json',
-      '.ts',
-      '.tsx',
-    ],
-  });
-  const rootDir = options.rootDir ?? process.cwd();
-  
-  const ignoreEdge = new Set(Module.builtinModules);
-
-  
-  const unresolvedEdgeQueue = [];  
-
-  
-  const unparsedFileQueue = [];
-  
-  const seenFiles = new Set();
-
-  const edges = new Set();
-  const files = new Map();
-
-  const readFile = (fileName) => {
-    return new Promise((resolve, reject) => {
-      return fileSystem.readFile(
-        fileName,
-        { encoding: 'utf8' },
-        (err, result) => {
-          if (err) {
-            return reject(err);
-          }
-
-          return resolve(result);
-        }
-      );
-    });
-  };
-
-  const resolve = (spec,fromId) => {
-    const ctx = {};
-
-    return new Promise((resolve, reject) =>
-      resolver.resolve({}, fromId, spec, ctx, (err, resolved) => {
-        if (err) {
-          return reject(err);
-        }
-
-        return resolve({ ctx, resolved });
-      })
-    );
-  };
-
-  // We can't let this throw because nothing will register a catch handler.
-  const parseFile = async (fileName) => {
-    try {
-      const sourceContent = await readFile(fileName);
-      const loader = loaderForPath(fileName);
-
-      if (!loader) {
-        // We can't figure out a loader so let's just treat it as a leaf node
-        files.set(fileName, { content: sourceContent, id: fileName });
-        return;
-      }
-
-      const buildResult = await esbuild.build({
-        bundle: true,
-        define: {
-          // TODO: Dynamic env vars
-          'proess.env.NODE_ENV': JSON.stringify('development'),
-        },
-        format: 'esm',
-        metafile: true,
-        platform: 'neutral',
-        plugins: [
-          {
-            name: 'capture-edges',
-            setup: (build) => {
-              build.onResolve({ filter:  }, (args) => {
-                if (!ignoreEdge.has(args.path)) {
-                  unresolvedEdgeQueue.push({
-                    fromId: fileName,
-                    fromContext: args.resolveDir,
-                    kind: args.kind,
-                    toSpec: args.path,
-                  });
-                }
-
-                // Mark everythign as external. We're only using esbuild to transform
-                // on a file-by-file basis and capture dependencies.
-                return { external: true };
-              });
-            },
-          },
-        ],
-        // sourcemap: true,
-        // sourcesContent: true,
-        stdin: {
-          contents: sourceContent,
-          resolveDir: Path.dirname(fileName),
-          loader: loaderForPath(fileName),
-          sourcefile: fileName,
-        },
-        // TODO: Dynamic target
-        target: 'node14',
-        treeShaking: true,
-        write: false,
-      });
-
-      const content = buildResult.outputFiles[0].text;
-
-      files.set(fileName, { content, id: fileName });
-    } catch (err) {
-      console.error('parseFile error', err);
-    }
-  };
-
-  // We can't let this throw because nothing will register a catch handler.
-  const resolveEdge = async (edge) => {
-    try {
-      const { resolved } = await resolve(edge.toSpec, edge.fromContext);
-
-      // TODO: We need special handling for `false` files and proper error handling
-      // for files that failed to resolve.
-      invariant(resolved, 'All files must successfully resolve (for now)');
-
-      // Record the resolved edge.
-      // TODO: Make sure we don't record the same logical edge twice.
-      edges.add({
-        fromId: edge.fromId,
-        toId: resolved,
-        kind: edge.kind,
-      });
-      unparsedFileQueue.push(resolved);
-    } catch (err) {
-      console.error('resolveEdge error', err);
-    }
-  };
-
-  const promises = new Set<Promise<unknown>>();
-  const track = (op: Promise<unknown>) => {
-    promises.add(op);
-    op.finally(() => promises.delete(op));
-  };
-
-  for (const entrypointSpec of entrypoint) {
-    unresolvedEdgeQueue.push({
-      fromId: '<root>',
-      fromContext: rootDir,
-      toSpec: entrypointSpec,
-      kind: 'entry-point',
-    });
+  const graphs = [];
+  for (let i=0; i<entryPoints.length; i++) {
+    const entryPoint = entryPoints[i];
+    graphs.push(await processEntryPoint(entryPoint));
   }
-
-  while (unparsedFileQueue.length || unresolvedEdgeQueue.length) {
-    while (unparsedFileQueue.length) {
-      const unparsedFile = unparsedFileQueue.shift()!;
-
-      if (!seenFiles.has(unparsedFile)) {
-        seenFiles.add(unparsedFile);
-
-        track(parseFile(unparsedFile));
-      }
-    }
-
-    while (unresolvedEdgeQueue.length) {
-      const unresolvedEdge = unresolvedEdgeQueue.shift()!;
-
-      track(resolveEdge(unresolvedEdge));
-    }
-
-    while (promises.size) {
-      await Promise.race(promises);
-    }
-  }
-
-  return { edges, files };
+  return graphs;
 }
-
-function loaderForPath(fileName: string): Loader | undefined {
-  const ext = Path.extname(fileName).slice(1);
-  switch (ext) {
-    case 'js':
-    case 'mjs':
-    case 'cjs':
-      return 'js';
-    case 'jsx':
-    case 'ts':
-    case 'tsx':
-    case 'json':
-    case 'css':
-      return ext;
-  }
-}
-
-module.exports = async function(dir) {
-  const graph = await buildGraph(["./src/dev"], {});
-  console.timeEnd("buildGraph");
-
-  console.log("Found %d modules with %d edges", graph.files.size, graph.edges.size);
-}*/
